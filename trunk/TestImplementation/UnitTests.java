86107ddaa5e6f682e2d15320fab2f29e6a17ab53
jared771@gmail.com <jared771@gmail.com@4f5dc209-3832-2cd5-a797-2146d41712b0>
2011-10-10 02:24:42 +0000

    Added UnitTests class to test implementation code.

---
+++ trunk/TestImplementation/UnitTests.java
@@ -0,0 +1,17 @@
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Carrier ac = new Carrier(0, 4, 'a', 'a');
+		System.out.println(ac.isSunk());
+		System.out.println(ac.getName());
+		
+		Cruiser cr = new Cruiser(0, 0, 'a', 'd');
+		System.out.println(cr.isSunk());
+		System.out.println(cr.getName());
+	}
+
+}
