bc508fdb2c3011ab07e454fb251581342d3a5617
fox7334@hotmail.com <fox7334@hotmail.com@4f5dc209-3832-2cd5-a797-2146d41712b0>
2011-10-23 16:33:26 +0000

    ShipNode class, slightly modified from the Node class

---
+++ trunk/TestImplementation/Node/ShipNode.java
@@ -0,0 +1,49 @@
+/**
+@author Marc Antoine Gosselin-Lavigne
+@author Jared Hanson
+@date October 8, 2011
+*/
+
+public class ShipNode{
+	private int col;
+	private char row;
+	private boolean isHit = false;
+
+	/**@param int xIn x coordinate of the ShipNode as an integer
+	@param char yIn y coordinate of the ShipNode as a char*/
+	public ShipNode(int xIn, char yIn){
+		col = xIn;
+		row = yIn;
+		isHit = false;
+	}
+
+	/**@param colIn the X-coordinate of the cell that we are checking
+	@param the Y-coordinate of the cell that we are checking
+	@return true if this ShipNode is at the position specified by the argument Node*/
+	public boolean isAt(int colIn, int rowIn){
+		if ((this.col == colIn) && (this.row == rowIn))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	
+	public int getCol()
+	{
+		return col;
+	}
+	
+	public char getRow()
+	{
+		return row;
+	}
+
+	/**@return whether or not the ship node has been hit by the opposite player*/
+	public boolean hasBeenHit()
+	{
+		return isHit;
+	}
+}
