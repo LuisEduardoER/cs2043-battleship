a572cdf4d65b12d8e76a357db31ce6d0534ab6ee
fox7334 <fox7334@hotmail.com>
2011-10-23 16:36:07 +0000

    Added Javadocs comment.

--- TestImplementation/Ship/Carrier.java
+++ TestImplementation/Ship/Carrier.java
@@ -1,9 +1,22 @@
+/**
+@author Marc Antoine Gosselin-Lavigne
+@author Jared Hanson
+@date October 8, 2011
+*/
 
 public class Carrier extends Ship {
 
+
+
 	public Carrier(int xStart, int xEnd, char yStart, char yEnd) {
+
 		
+
 		super(xStart, xEnd, yStart, yEnd);
+
 		name = "AC";
+
 	}
+
 }
+
