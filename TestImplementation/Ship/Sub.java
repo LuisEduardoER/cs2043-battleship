4d039a36dcbfc849451db40e1d74c36be8ebefdd
fox7334 <fox7334@hotmail.com>
2011-10-23 16:37:28 +0000

    Added Javadocs comments, changed yStart and yEnd to be consistent with design documents.

--- TestImplementation/Ship/Sub.java
+++ TestImplementation/Ship/Sub.java
@@ -1,7 +1,12 @@
+/**
+@author Marc Antoine Gosselin-Lavigne
+@author Jared Hanson
+@date October 8, 2011
+*/
 
 public class Sub extends Ship {
 
-	public Sub(int xStart, int xEnd, char yStart, char yEnd) {
+	public Sub(int xStart, int xEnd, int yStart, int yEnd) {
 		
 		super(xStart, xEnd, yStart, yEnd);
 		name = "SB";
