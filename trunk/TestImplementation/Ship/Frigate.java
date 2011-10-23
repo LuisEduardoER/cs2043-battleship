fa1f540353e8fcf677533a787c305ed0dc005050
fox7334@hotmail.com <fox7334@hotmail.com@4f5dc209-3832-2cd5-a797-2146d41712b0>
2011-10-23 16:38:14 +0000

    Added Javadocs comments, changed yStart and yEnd to int to reflect design documents.

--- trunk/TestImplementation/Ship/Frigate.java
+++ trunk/TestImplementation/Ship/Frigate.java
@@ -1,7 +1,12 @@
+/**
+@author Marc Antoine Gosselin-Lavigne
+@author Jared Hanson
+@date October 8, 2011
+*/
 
 public class Frigate extends Ship {
 
-	public Frigate(int xStart, int xEnd, char yStart, char yEnd) {
+	public Frigate(int xStart, int xEnd, int yStart, int yEnd) {
 		
 		super(xStart, xEnd, yStart, yEnd);
 		name = "FR";
